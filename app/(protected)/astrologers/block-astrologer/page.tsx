f69c6e9bae3e71fa5c04944a4b9ca87f0a399fb6
Dilip Vishwakarma <dilipvishwakarma7878@gmail.com>
2025-09-03 10:17:04 +0530

    Block astrologer completed

---
+++ app/(protected)/astrologers/block-astrologer/page.tsx
@@ -0,0 +1,24 @@
+import { Loader } from "@/components/ui-kit/Loader";
+import React, { Suspense } from "react";
+import { apiServices } from "@/lib/api.services";
+import { manage_astrologer } from "@/lib/api-endpoints";
+import { TextH1 } from "@/components/ui-kit/TextH1";
+import PageBase from "./toolkit/page-base";
+
+const AstrologerList = async () => {
+  const response = await apiServices(manage_astrologer, "get");
+
+  return (
+    <Suspense fallback={<Loader />}>
+      <TextH1 className="mt-3">
+        Block Astrologer
+      </TextH1>
+      <PageBase
+        initialData={response.data}
+        initialPagination={response.pagination}
+      />
+    </Suspense>
+  );
+};
+
+export default AstrologerList;
