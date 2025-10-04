73d9136a30d94c61b8b5a3e05678bf6bf917d536
Dilip Vishwakarma <dilipvishwakarma7878@gmail.com>
2025-10-04 12:43:48 +0530

    added nextjs-toploader

--- src/provider/main-provider.tsx
+++ src/provider/main-provider.tsx
@@ -2,10 +2,12 @@
 import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
+import NextTopLoader from 'nextjs-toploader';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <SessionProvider>
+      <NextTopLoader color="#E25016" showSpinner={false} zIndex={9999} />
       {children}
       <Toaster richColors />
     </SessionProvider>
