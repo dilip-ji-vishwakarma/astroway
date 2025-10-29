e2099d033697dccc5728c5d566bb781142a6265e
Dilip Vishwakarma <dilipvishwakarma7878@gmail.com>
2025-10-29 17:42:46 +0530

    updated pagination in banner management

--- app/(protected)/banner-management/page.tsx
+++ app/(protected)/banner-management/page.tsx
@@ -1,5 +1,3 @@
-import { banner } from "@/lib/api-endpoints";
-import { apiServices } from "@/lib/api.services";
 import { Metadata } from "next";
 import React, { Suspense } from "react";
 import { BaseHeader } from "./toolkit/base-header";
@@ -10,14 +8,10 @@ export const metadata: Metadata = {
 };
 
 const BannerManagement = async () => {
-  const response = await apiServices(banner, "get");
   return (
     <Suspense>
       <BaseHeader />
-      <PageBase
-        initialData={response.data}
-        initialPagination={response.pagination}
-      />
+      <PageBase />
     </Suspense>
   );
 };
--- app/(protected)/banner-management/toolkit/page-base.tsx
+++ app/(protected)/banner-management/toolkit/page-base.tsx
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useState } from "react";
-import { useDataMutations } from "../hook/use-data-mutations";
-import { MetaPagination } from "@/components/ui-kit/meta-pagination/meta-pagination";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -18,195 +16,183 @@ import {
   Trash2,
   CheckCircle,
   Circle,
+  Loader2,
 } from "lucide-react";
 import { formatSingleDate, getImageUrl } from "@/lib/utils";
 import Image from "next/image";
 import { UpdateBanner } from "./update-banner";
-import { apiServices } from "@/lib/api.services";
-import { toast } from "sonner";
-import { banner } from "@/lib/api-endpoints";
+import { useDataMutation } from "../hook/use-data-mutation";
+import { MetaPagination } from "@/components/ui-kit/meta-paginations/meta-pagination";
+import { Label } from "@/components/ui/label";
 
-type PageBaseProps = {
-  initialData: any[];
-  initialPagination: {
-    total: number;
-    page: number;
-    limit: number;
-    totalPages: number;
-  };
-};
-
-export const PageBase = ({ initialData, initialPagination }: PageBaseProps) => {
+export const PageBase = () => {
   const [open, setOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<any | null>(null);
-  const [deletingId, setDeletingId] = useState<number | null>(null);
-  const [tooglegId, setTooglegId] = useState<number | null>(null);
-  const { data, pagination, handlePageChange } = useDataMutations(
-    initialData,
-    initialPagination
-  );
-
-  const handleDelete = async (id: any) => {
-    setDeletingId(id);
-    try {
-      const response = await apiServices(`${banner}/${id}`, "delete");
-      if (response?.success === true) {
-        toast.success(response.message);
-        window.location.reload();
-      } else {
-        toast.error(response.message);
-      }
-    } catch (error: any) {
-      toast.error(error?.message || "Something went wrong");
-    } finally {
-      setDeletingId(null);
-    }
-  };
-
-  const handleToogle = async (id: number, currentStatus: boolean) => {
-    setTooglegId(id);
-    try {
-      const response = await apiServices(`${banner}/status/${id}`, "patch", {
-        isActive: !currentStatus,
-      });
-
-      if (response?.success === true) {
-        toast.success(response.message);
-        window.location.reload();
-      } else {
-        toast.error(response.message);
-      }
-    } catch (error: any) {
-      toast.error(error?.message || "Something went wrong");
-    } finally {
-      setTooglegId(null);
-    }
-  };
+  const {
+    data,
+    pagination,
+    loading,
+    handlePageChange,
+    handleLimitChange,
+    handleDelete,
+    handleToogle,
+    deletingId,
+    tooglegId,
+  } = useDataMutation();
 
   return (
-    <div className="mt-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data.map((item, index) => (
-          <div
-            key={index}
-            className="relative group rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-1"
-          >
-            <DropdownMenu>
-              <DropdownMenuTrigger className="absolute top-3 right-3 bg-black/40 text-white rounded-full p-2 opacity-0 group-hover:opacity-100 transition duration-300 z-10 cursor-pointer">
-                <EllipsisVertical size={18} />
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                align="end"
-                className="w-44 rounded-xl shadow-xl border border-slate-100"
-              >
-                <DropdownMenuItem
-                  className="flex items-center gap-2 text-slate-700 hover:bg-slate-100"
-                  onSelect={() => {
-                    setOpen(true);
-                    setSelectedItem(item);
-                  }}
+    <div className="mt-5 relative">
+      <Label className="text-md font-semibold">{`${pagination.total} Listings`}</Label>
+      {loading ? (
+        <div className="flex justify-center items-center h-48">
+          <Loader2 className="animate-spin text-muted-foreground" size={32} />
+        </div>
+      ) : (
+        <div
+          className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4 transition-opacity duration-300 ${
+            loading ? "opacity-50 pointer-events-none" : "opacity-100"
+          }`}
+        >
+          {data.map((item, index) => (
+            <div
+              key={index}
+              className="relative group rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:-translate-y-1 bg-white"
+            >
+              {/* Dropdown Menu */}
+              <DropdownMenu>
+                <DropdownMenuTrigger className="absolute top-3 right-3 bg-black/40 text-white rounded-full p-2 opacity-0 group-hover:opacity-100 transition duration-300 z-10 cursor-pointer">
+                  <EllipsisVertical size={18} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  align="end"
+                  className="w-44 rounded-xl shadow-xl border border-slate-100"
                 >
-                  <Pencil size={16} className="text-blue-500" />
-                  Edit Banner
-                </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="flex items-center gap-2 text-slate-700 hover:bg-slate-100"
+                    onSelect={() => {
+                      setOpen(true);
+                      setSelectedItem(item);
+                    }}
+                  >
+                    <Pencil size={16} className="text-blue-500" />
+                    Edit Banner
+                  </DropdownMenuItem>
 
-                <DropdownMenuSeparator />
+                  <DropdownMenuSeparator />
 
-                <DropdownMenuItem
-                  className="flex items-center gap-2 cursor-pointer hover:bg-slate-100"
-                  onSelect={(e) => {
-                    e.preventDefault();
-                    handleToogle(item.id, item.isActive);
-                  }}
-                >
-                  {tooglegId === item.id ? (
-                    <span className="text-xs text-slate-500">Loading...</span>
-                  ) : item.isActive ? (
-                    <>
-                      <CheckCircle size={16} className="text-green-500" />
-                      <span>Active</span>
-                    </>
-                  ) : (
-                    <>
-                      <Circle size={16} className="text-gray-400" />
-                      <span>Inactive</span>
-                    </>
-                  )}
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  className="flex items-center gap-2 text-red-600 hover:bg-red-50 cursor-pointer"
-                  onSelect={(e) => {
-                    e.preventDefault();
-                    handleDelete(item.id);
-                  }}
-                >
-                  {deletingId === item.id ? (
-                    <span>Deleting...</span>
-                  ) : (
-                    <>
-                      <Trash2 size={16} />
-                      <span>Delete</span>
-                    </>
-                  )}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+                  <DropdownMenuItem
+                    className="flex items-center gap-2 cursor-pointer hover:bg-slate-100"
+                    onSelect={(e) => {
+                      e.preventDefault();
+                      handleToogle(item.id, item.isActive);
+                    }}
+                  >
+                    {tooglegId === item.id ? (
+                      <span className="text-xs text-slate-500">Loading...</span>
+                    ) : item.isActive ? (
+                      <>
+                        <CheckCircle size={16} className="text-green-500" />
+                        <span>Active</span>
+                      </>
+                    ) : (
+                      <>
+                        <Circle size={16} className="text-gray-400" />
+                        <span>Inactive</span>
+                      </>
+                    )}
+                  </DropdownMenuItem>
 
-            {item.url ? (
-              <Image
-                src={getImageUrl(item.url)}
-                alt="banner"
-                className="w-full h-60 object-cover transition-transform duration-500 group-hover:scale-105"
-              width={150}
+                  <DropdownMenuSeparator />
+
+                  <DropdownMenuItem
+                    className="flex items-center gap-2 text-red-600 hover:bg-red-50 cursor-pointer"
+                    onSelect={(e) => {
+                      e.preventDefault();
+                      handleDelete(item.id);
+                    }}
+                  >
+                    {deletingId === item.id ? (
+                      <span>Deleting...</span>
+                    ) : (
+                      <>
+                        <Trash2 size={16} />
+                        <span>Delete</span>
+                      </>
+                    )}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              {/* Image */}
+              {item.url ? (
+                <Image
+                  src={getImageUrl(item.url)}
+                  alt="banner"
+                  width={150}
                   height={150}
-              />
-            ) : (
-              <Image
-                src="/images/bannerImage_placeholder.png"
-                alt={item.channel}
-                width={500}
-                height={240}
-                className="w-full h-60 object-cover transition-transform duration-500 group-hover:scale-105"
-              />
-            )}
+                  className="w-full h-60 object-cover transition-transform duration-500 group-hover:scale-105"
+                />
+              ) : (
+                <Image
+                  src="/images/bannerImage_placeholder.png"
+                  alt="placeholder"
+                  width={500}
+                  height={240}
+                  className="w-full h-60 object-cover transition-transform duration-500 group-hover:scale-105"
+                />
+              )}
 
-            <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent"></div>
-            <div className="absolute bottom-0 w-full backdrop-blur-md bg-white/20 p-4 text-white">
-              <div className="flex justify-between items-center border-b border-white/20 pb-2 mb-2">
-                <span className="flex items-center gap-2 text-sm">
-                  <Calendar1 size={16} /> From
-                </span>
-                <span className="text-sm font-semibold">
-                  {formatSingleDate(item.fromDate, true)}
-                </span>
-              </div>
+              {/* Banner Details */}
+              <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent"></div>
+              <div className="absolute bottom-0 w-full backdrop-blur-md bg-white/20 p-4 text-white">
+                <div className="flex justify-between items-center border-b border-white/20 pb-2 mb-2">
+                  <span className="flex items-center gap-2 text-sm">
+                    <Calendar1 size={16} /> From
+                  </span>
+                  <span className="text-sm font-semibold">
+                    {formatSingleDate(item.fromDate, true)}
+                  </span>
+                </div>
 
-              <div className="flex justify-between items-center border-b border-white/20 pb-2 mb-2">
-                <span className="flex items-center gap-2 text-sm">
-                  <Calendar1 size={16} /> To
-                </span>
-                <span className="text-sm font-semibold">
-                  {formatSingleDate(item.toDate, true)}
-                </span>
-              </div>
+                <div className="flex justify-between items-center border-b border-white/20 pb-2 mb-2">
+                  <span className="flex items-center gap-2 text-sm">
+                    <Calendar1 size={16} /> To
+                  </span>
+                  <span className="text-sm font-semibold">
+                    {formatSingleDate(item.toDate, true)}
+                  </span>
+                </div>
 
-              <div className="flex justify-between items-center">
-                <span className="flex items-center gap-2 text-sm">
-                  <Video size={16} /> Type
-                </span>
-                <span className="text-sm font-semibold">{item.bannerType}</span>
+                <div className="flex justify-between items-center">
+                  <span className="flex items-center gap-2 text-sm">
+                    <Video size={16} /> Type
+                  </span>
+                  <span className="text-sm font-semibold">
+                    {item.bannerType}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-end w-full mt-6">
-        <MetaPagination
-          pagination={pagination}
-          onPageChange={handlePageChange}
-        />
-      </div>
+          ))}
+        </div>
+      )}
+      {/* Pagination */}
+      {!loading && data.length > 0 && (
+        <div className="flex justify-between items-center w-full mt-6">
+          <MetaPagination
+            total={pagination.total}
+            value={pagination.page}
+            recordPerPage={pagination.limit}
+            onChange={handlePageChange}
+          />
+          <MetaPagination.PerPage
+            value={pagination.limit}
+            onChange={handleLimitChange}
+          />
+        </div>
+      )}
+
+      {/* Update Modal */}
       <UpdateBanner
         open={open}
         onOpenChange={setOpen}
