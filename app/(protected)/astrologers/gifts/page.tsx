647c110c6d8020c0862ce6849244848ca5e3a12b
Dilip Vishwakarma <dilipvishwakarma7878@gmail.com>
2025-10-29 13:55:53 +0530

    updated pagination in gift

--- app/(protected)/astrologers/gifts/page.tsx
+++ app/(protected)/astrologers/gifts/page.tsx
@@ -1,6 +1,4 @@
 import { Loader } from '@/components/ui-kit/Loader';
-import { gifts } from '@/lib/api-endpoints';
-import { apiServices } from '@/lib/api.services';
 import { Metadata } from 'next'
 import React, { Suspense } from 'react'
 import { PageBase } from './toolkit/page-base';
@@ -11,11 +9,10 @@ export const metadata: Metadata = {
 }
 
 const Gifts = async () => {
-    const response = await apiServices(gifts, "get");
   return (
     <Suspense fallback={<Loader />}>
         <BaseHeader />
-      <PageBase initialData={response.data} initialPagination={response.pagination} />
+      <PageBase />
     </Suspense>
   )
 }
