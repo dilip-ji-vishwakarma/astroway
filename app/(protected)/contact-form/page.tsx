2aa75fbc0efdccf9f33b6ebbffc33a4d9756b10a
Dilip Vishwakarma <dilipvishwakarma7878@gmail.com>
2025-11-15 17:52:08 +0530

    contact form page created

---
+++ app/(protected)/contact-form/page.tsx
@@ -0,0 +1,15 @@
+import { Loader } from "@/components/ui-kit/Loader";
+import React, { Suspense } from "react";
+import { PageBase } from "./toolkit/page-base";
+import { TextH1 } from "@/components/ui-kit/TextH1";
+
+const Page = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <TextH1>Contact Form</TextH1>
+      <PageBase />
+    </Suspense>
+  );
+};
+
+export default Page;
---
+++ app/(protected)/contact-form/toolkit/page-base.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PageBase = () => {
+  return (
+    <div>
+      Contact form
+    </div>
+  )
+}
